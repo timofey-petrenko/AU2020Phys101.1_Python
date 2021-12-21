4dc30bdf1baeb03756834129817934009c1d99e4
timofey <timofey.petrenko.acad@gmail.com>
2021-12-21 13:13:54 +0300

    Finally!!!

--- Python/primality_test.py
+++ Python/primality_test.py
@@ -6,28 +6,36 @@ from random import randint
 
 def is_prime(num):
     """Говорит, верно ли, что число простое"""
-    try:
-        if num <= 1:
-            return False
-        if num == 2 or num == 3:
-            return True
-        if num % 2 == 0 or num % 3 == 0:
-            return False
-        i = 5
-        k = 2
-        while i <= math.ceil(num ** 0.5):
-            if num % i == 0:
-                return False
-            i += k
-            k = 6 - k
+    if num <= 1:
+        return False
+    if num == 2 or num == 3:
         return True
-    except ValueError:
-        print('Просили ли же: ЦЕЛОЕ ЧИСЛО!')
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    k = 2
+    while i <= math.ceil(num ** 0.5):
+        if num % i == 0:
+            return False
+        i += k
+        k = 6 - k
+    return True
+   
 
-numbers = []
+nums = []
 for _ in range(100):
     value = randint(-100_000, 100_000)
-    numbers.append(value)
+    nums.append(value)
 
-for number in numbers:    
-    print(number, ':', is_prime(number))
+
+for number in nums:
+    if type(number) == int: 
+        print(number, ':', is_prime(number))
+    else:
+        print('Дайте мне число в формате int')
+
+my_number = input('Введите свое целое число')
+if type(my_number) == int:
+    print(is_prime(my_number))
+else:
+    print('Дайте мне число в формате int')
