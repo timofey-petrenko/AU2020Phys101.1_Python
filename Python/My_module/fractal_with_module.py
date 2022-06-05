478a00b8c9d5d61ba82fddc941187b3ce8cb97e9
timofey-petrenko <90569092+timofey-petrenko@users.noreply.github.com>
2022-06-05 19:59:18 +0300

    Create fractal_with_module.py

---
+++ Python/My_module/fractal_with_module.py
@@ -0,0 +1,13 @@
+import turtle
+import sierpinski
+
+size = 800
+min_L = 10
+const = 3 ** 0.5 / 2
+
+t = turtle.Turtle()
+t.hideturtle()
+t.penup()
+t.speed(0)
+Sierpinski(size, -size / 2, -size * const / 2)
+turtle.done()
