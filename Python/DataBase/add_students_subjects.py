9ded1dfe142559cfe600fe51bba213aa7cc7234b
timofey-petrenko <90569092+timofey-petrenko@users.noreply.github.com>
2022-06-06 11:54:01 +0300

    Update add_students_subjects.py

--- Python/DataBase/add_students_subjects.py
+++ Python/DataBase/add_students_subjects.py
@@ -2,17 +2,17 @@ import cs50
 from cs50 import SQL
 
 
-open('list_of_classes.db', 'w')
-db = SQL('sqlite:///list_of_classes.db')
+open('list_of_classes.db', 'w') #открываем файл
+db = SQL('sqlite:///list_of_classes.db') #работаем с SQLite через Python
 
 
-student_name = input('Type in the full name (and the family name) of the student\n')
+student_name = input('Type in the full name (and the family name) of the student\n') #получаем имя студента
 list_of_subjects = input('Type in all the subjects this students has participated in, \
-                         separate them by commmas\n').split(',')
+                         separate them by commmas\n').split(',') #получаем список предметов, в которых он участвовал
 
 
-id = db.execute('INSERT INTO students (student) VALUES(?)', student_name)
-for subject in list_of_subjects:
+id = db.execute('INSERT INTO students (student) VALUES(?)', student_name) #добавляем его в таблицу студентов
+for subject in list_of_subjects: #добавляем предметы в таблицу предметов, соотносяя со студентом
     id_class = db.execute('INSERT INTO classes (class) VALUES(?)', subject)
 
-close('list_of_classes.db')
+close('list_of_classes.db') #не забываем закрыть файл!
