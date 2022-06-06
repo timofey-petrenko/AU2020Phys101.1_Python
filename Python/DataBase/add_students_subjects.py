b642861143618628a1ea39b9717a581dc20a7e43
timofey-petrenko <90569092+timofey-petrenko@users.noreply.github.com>
2022-06-06 14:59:07 +0300

    Update add_students_subjects.py

--- Python/DataBase/add_students_subjects.py
+++ Python/DataBase/add_students_subjects.py
@@ -14,6 +14,6 @@ list_of_subjects = input('Type in all the subjects this students has participate
 id = db.execute('INSERT INTO students (student) VALUES(?)', student_name) #добавляем его в таблицу студентов
 for subject in list_of_subjects: #добавляем предметы в таблицу предметов, соотносяя со студентом
     id_class = db.execute('INSERT INTO classes (class) VALUES(?)', subject)
-print('New dats added successfully.')
+print('New data added successfully.')
 
 close('list_of_classes.db') #не забываем закрыть файл!
