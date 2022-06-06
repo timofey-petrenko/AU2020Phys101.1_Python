1ec3ca318922aa781cd0b7c07ae969bbda09a227
timofey-petrenko <90569092+timofey-petrenko@users.noreply.github.com>
2022-06-06 11:47:34 +0300

    Update create_table.py

--- Python/DataBase/create_table.py
+++ Python/DataBase/create_table.py
@@ -2,7 +2,7 @@ import cs50
 from cs50 import SQL
 
 open('list_of_classes.db', 'w').close()
-db = SQL('sqlite:///exams.db')
+db = SQL('sqlite:///list_of_classes.db')
 
 db.execute('CREATE TABLE students (id INTEGER, student TEXT, PRIMARY KEY(id))')
 db.execute('CREATE TABLE classes (class_id INTEGER, class TEXT, FOREIGN KEY(class_id) REFERENCES  students(id))')
