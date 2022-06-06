72f6ec6bd09f6f1690c8566dc595d1aaf9b66517
timofey-petrenko <90569092+timofey-petrenko@users.noreply.github.com>
2022-06-06 11:51:57 +0300

    Update create_table.py

--- Python/DataBase/create_table.py
+++ Python/DataBase/create_table.py
@@ -1,8 +1,8 @@
-import cs50
+import cs50    #вот эту вот библиотеку https://cs50.readthedocs.io/libraries/cs50/python/ - она упрощает работу с SQLite через Python
 from cs50 import SQL
 
-open('list_of_classes.db', 'w').close()
-db = SQL('sqlite:///list_of_classes.db')
+open('list_of_classes.db', 'w').close() #создаем файл
+db = SQL('sqlite:///list_of_classes.db') #работаем с ним через SQLite
 
-db.execute('CREATE TABLE students (id INTEGER, student TEXT, PRIMARY KEY(id))')
-db.execute('CREATE TABLE classes (class_id INTEGER, class TEXT, FOREIGN KEY(class_id) REFERENCES  students(id))')
+db.execute('CREATE TABLE students (id INTEGER, student TEXT, PRIMARY KEY(id))') #создаем таблицу студентов
+db.execute('CREATE TABLE classes (class_id INTEGER, class TEXT, FOREIGN KEY(class_id) REFERENCES  students(id))') #создаем таблицу предметов
